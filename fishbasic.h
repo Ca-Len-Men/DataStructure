5b92b3d73decb44baf0514d239eb65dcae0a8473
Ca-Len-Men <88229844+Ca-Len-Men@users.noreply.github.com>
2022-01-28 11:51:34 +0700

    Update fishbasic.h

--- fishbasic.h
+++ fishbasic.h
@@ -1,12 +1,16 @@
 #ifndef _FISH_BASIC_
 #define _FISH_BASIC_
 
+#include "painttree.h"
 #include<iostream>
 #include<algorithm>
+#include<iomanip>
 
 template <class data> class stack;
 template <class data> class queue;
 template <class data> class SList;
+template <class key, class value> class bucket;
+template <class key, class value> class AVLTree;
 //==================================================
 //==========          Stack          ==========
 template <class data>
@@ -15,7 +19,7 @@ class stack {
 	public:
 		data info;
 		node* next = nullptr;
-		node(const data& var) { info = var; }
+		node(const data& var) : info{ var } {};
 	};
 	
 	node* top = nullptr;
@@ -152,7 +156,7 @@ class queue {
 	public:
 		data info;
 		node* next = nullptr;
-		node(const data& var) { info = var; }
+		node(const data& var) : info { var } {};
 	};
 
 	node* head = nullptr;
@@ -806,5 +810,412 @@ SList<data>::SList(const SList& var)
 	for (node* i = var.head->next; i; i = i->next, tail = tail->next)
 		tail->next = new node(i->info);
 }
+//==================================================
+//==========          Bucket          ==========
+enum class status {
+	isEmpty,
+	isFull,
+	notValue,
+	notFound
+};
+void testBucket(status Is)
+{
+	switch (Is)
+	{
+		case status::isEmpty: printf("Empty !");	break;
+		case status::isFull: printf("Ton tai !");	break;
+		case status::notValue: printf("Khong co gia tri !");	break;
+		case status::notFound: printf("Khong ton tai !");	break;
+	}
+}
+
+template <class key, class value>
+class bucket {
+	key code{};
+	value info{};
+	status valid{ status::isEmpty };
+
+public:
+	//Khởi tạo
+	bucket(status Is = status::isEmpty) : valid { Is }
+	{	//Empty or NotFound
+		if (valid == status::isFull || valid == status::notValue)
+			valid = status::isEmpty;
+	};
+	//Full
+	bucket(const key& newCode, const value& newInfo) :
+		code{ newCode },
+		info{ newInfo },
+		valid{ status::isFull }{};
+	//Not Value
+	bucket(const key& newCode) :
+		code{ newCode },
+		valid{ status::notValue },
+		info{}{};
+	//Copy
+	bucket(const bucket& newBucket) :
+		valid{ newBucket.valid },
+		code{ newBucket.code },
+		info{ newBucket.info }{};
+
+	//Phương thức
+	key getKey() const			{ return code; }
+	value getValue() const		{ return info; }
+	status getStatus() const	{ return valid; }
+	//Full
+	void change(const key& newCode, const value& newInfo)
+	{
+		code = newCode;
+		info = newInfo;
+		valid = status::isFull;
+	}
+	//Not Value
+	void change(const key& newCode)
+	{
+		code = newCode;
+		valid = status::notValue;
+	}
+
+	bool isEmpty() const	{ return valid == status::isEmpty; };
+	bool isFull() const		{ return valid == status::isFull; };
+	bool notValue() const	{ return valid == status::notValue; };
+	bool notFound() const	{ return valid == status::notFound; };
+
+	//Toán tử
+	bucket& operator=(const bucket& newBucket) {
+		code = newBucket.code;
+		info = newBucket.info;
+		valid = newBucket.valid;
+		return *this;
+	}
+
+	bool operator==(const bucket& newBucket) const { return code == newBucket.code; };
+	bool operator>(const bucket& newBucket) const { return code > newBucket.code; };
+	bool operator<(const bucket& newBucket) const { return code < newBucket.code; };
+};
+//==================================================
+//==========          AVL Tree          ==========
+template <class key, class value>
+class AVLTree {
+	class node {
+	public:	//+ : nghiêng trái			- : nghiêng phải
+		node* left, * right;
+		bucket<key, value> info;
+		unsigned char height;
+
+		//Khởi tạo
+		node(const bucket<key, value>& newInfo) :
+			info{ newInfo },
+			left{ nullptr },
+			right{ nullptr },
+			height{ 1 }
+			{};
+
+		//Phương thức
+		void countHeight() {	//Tính lại chiều cao
+			unsigned char leftHeight = left == nullptr ? 0 : left->height;
+			unsigned char rightHeight = right == nullptr ? 0 : right->height;
+			height = std::max(leftHeight, rightHeight) + 1;
+		}
+		//Gán mới một bucket
+		void copyBucket(const bucket<key, value>& newBucket) {
+			info = newBucket;
+		}
+		//Tính độ lệch của hai cây con
+		char countBalance() {
+			char leftHeight = left == nullptr ? 0 : left->height;
+			char rightHeight = right == nullptr ? 0 : right->height;
+			return leftHeight - rightHeight;
+		}
+
+		//Hàm tĩnh
+		//Quay trái
+		static void rotateLeft(node*& root) {
+			if (root == nullptr)
+				return;
+
+			if (root->right == nullptr)
+				return;
+			//Quay trái
+			node* childLeft = root;
+			root = root->right;
+			childLeft->right = root->left;
+			root->left = childLeft;
+			//Tính lại chiều cao
+			childLeft->countHeight();
+			root->countHeight();
+		}
+
+		//Quay phải
+		static void rotateRight(node*& root) {
+			if (root == nullptr)
+				return;
+
+			if (root->left == nullptr)
+				return;
+			//Quay trái
+			node* childRight = root;
+			root = root->left;
+			childRight->left = root->right;
+			root->right = childRight;
+			//Tính lại chiều cao
+			childRight->countHeight();
+			root->countHeight();
+		}
+
+		//Cân bằng tại root
+		static void balanceRoot(node*& root) {
+			if (root == nullptr)
+				return;
+
+			char tilt = root->countBalance();
+			//Nghiêng phải -> quay trái
+			if (tilt < -1) {
+				if (root->right->countBalance() <= 0)	//Cây con phải lệch phải
+					rotateLeft(root);
+				else {	//Cây con phải lệch trái
+					rotateRight(root->right);
+					rotateLeft(root);
+				}
+			}
+			else if (tilt > 1) {//Nghiêng trái -> quay phải
+				if (root->left->countBalance() >= 0)	//Cây con trái lệch trái
+					rotateRight(root);
+				else {	//Cây con trái lệch phải
+					rotateLeft(root->left);
+					rotateRight(root);
+				}
+			}
+		}
+
+		//Tìm phần tử thế mạng ở cây con trái
+		static void poorChildLeft(node*& root) {
+			if (root == nullptr || root->left == nullptr)
+				return;
+
+			stack<node*&> st;
+			st.push(root->left);
+			while (st.get()->right != nullptr)
+				st.push(st.get()->right);
+
+			node*& temp = st.pop();
+			root->info = temp->info;
+			node* p = temp;
+			temp = temp->left;
+			delete p;
+
+			while (!st.isEmpty())
+				balanceRoot(st.pop());
+		}
+
+		//Tìm phần tử thế mạng ở cây con phải
+		static void poorChildRight(node*& root) {
+			if (root == nullptr || root->right == nullptr)
+				return;
+
+			stack<node*&> st;
+			st.push(root->right);
+			while (st.get()->left != nullptr)
+				st.push(st.get()->left);
+
+			node*& temp = st.pop();
+			root->info = temp->info;
+			node* p = temp;
+			temp = temp->right;
+			delete p;
+
+			while (!st.isEmpty())
+				balanceRoot(st.get());
+		}
+	};
+	//=====     Tree     =====
+	node* root;
+	int size, weightOut;
+
+	//Hàm tĩnh
+	static void pushRecursive(node*& root, const bucket<key, value>& newBucket);
+	static bool popRecursive(node*& root, bucket<key, value>& newBucket);
+	static void outputRecursiveLNR(node*& root, const int& weight, int count, bool leftChild);
+
+public:
+	//Khởi tạo
+	AVLTree(int setWeight = 10) :
+		root{ nullptr }, size{ 0 }, weightOut{ setWeight }
+	{
+		if (weightOut <= 0)
+			weightOut = 10;
+	};
+
+	//Phương thức
+	void paint() const;
+	void output() const;
+	void push(const bucket<key, value>& newBucket);
+	int length() const { return size; }
+	bucket<key, value> pop(const key& code);
+	bucket<key, value> find(const key& code) const;
+};
+
+template <class key, class value>
+bucket<key, value> AVLTree<key, value>::find(const key& code) const
+{
+	node* p = root;
+	while (p != nullptr)
+	{
+		if (p->info.getKey() == code)
+			return p->info;
+		else if (p->info.getKey() > code)	//Tìm sang cây con trái
+			p = p->left;
+		else
+			p = p->right;
+	}
+	//Không tìm thấy
+	return bucket<key, value>(status::notFound);
+}
+
+template <class key, class value>
+void AVLTree<key, value>::paint() const
+{
+	print_ascii_tree<node>(root);
+}
+
+template <class key, class value>
+bucket<key, value> AVLTree<key, value>::pop(const key& code)
+{
+	bucket<key, value> res{ code };
+	if (popRecursive(root, res)) {	//Tồn tại
+		size--;
+		return res;
+	}
+	
+	return bucket<key, value>(status::notFound);
+}
+
+template <class key, class value>
+void AVLTree<key, value>::push(const bucket<key, value>& newBucket)
+{
+	if (!newBucket.isFull())
+		return;
+	pushRecursive(root, newBucket);
+	size++;
+}
+
+template <class key, class value>
+void AVLTree<key, value>::outputRecursiveLNR(node*& root, const int& weight, int count, bool leftChild)
+{
+	if (root == nullptr)
+		return;
+
+	outputRecursiveLNR(root->left, weight, count + 1, true);
+
+	if (count)
+		for (int i = weight * count; i > 0; i--)
+			putchar(' ');
+	std::cout << (leftChild ? '/' : '\\');
+	std::cout << std::setw(weight - 2) << root->info.getKey() << "|\n";
+
+	outputRecursiveLNR(root->right, weight, count + 1, false);
+}
+
+template <class key, class value>
+bool AVLTree<key, value>::popRecursive(node*& root, bucket<key, value>& newBucket)
+{
+	if (root == nullptr)
+		return false;
+
+	bool getValue;
+	if (root->info == newBucket)
+	{
+		newBucket = root->info;
+		if (root->left == nullptr && root->right == nullptr)	//Nút lá
+		{
+			delete root;
+			root = nullptr;
+			return true;
+		}
+
+		//Tìm nút thế mạng
+		if (root->countBalance() >= 0)	//Nghiêng trái : tìm nút thế mạng bên cây con trái
+			node::poorChildLeft(root);
+		else
+			node::poorChildRight(root);
+		return true;
+	}
+	else if (newBucket < root->info)	//Đệ quy sang cây con trái
+		getValue = popRecursive(root->left, newBucket);
+	else
+		getValue = popRecursive(root->right, newBucket);//Đệ quy sang cây con phải
+
+	node::balanceRoot(root);
+	return getValue;
+}
+
+template <class key, class value>
+void AVLTree<key, value>::pushRecursive(node*& root, const bucket<key, value>& newBucket)
+{
+	if (root == nullptr)
+	{
+		root = new node(newBucket);
+		return;
+	}
+
+	if (root->info == newBucket)
+	{
+		root->info = newBucket;
+		return;
+	}
+	else if (root->info < newBucket)//Đệ quy sang phải
+		pushRecursive(root->right, newBucket);
+	else//Đệ quy sang trái
+		pushRecursive(root->left, newBucket);
+
+	//Cân bằng nút
+	root->countHeight();
+	node::balanceRoot(root);
+}
+
+template <class key, class value>
+void AVLTree<key, value>::output() const
+{
+	if (root == nullptr)
+	{
+		std::cout << "Tree is empty\n";
+		return;
+	}
+
+	outputRecursiveLNR(root->left, weightOut, 1, true);
+	std::cout << '|' << std::setw(weightOut - 2) << root->info.getKey() << "|\n";
+	outputRecursiveLNR(root->right, weightOut, 1, false);
+}
+//==================================================
+//==========          Red Black Tree          ==========
+template <class key, class value>
+class RBTree {
+	enum class colors { RED, BLACK };
+
+	class node {
+	public:
+		bucket<key, value> info;
+		node* left, * right, * parent;
+		colors color;
+
+		//Constructor
+		node(const bucket<key, value>& newBucket) {
+			info = newBucket;
+			left = right = parent = nullptr;
+			color = colors::RED;
+		}
+
+		//Method
+		void connectLeft(node* leftChild) {
+			left = leftChild;
+			leftChild->parent = this;
+		}
+
+		void connectRight(node* rightChild) {
+			right = rightChild;
+			rightChild->parent = this;
+		}
+	};
+};
 //==================================================
 #endif	//_FISH_BASIC_
